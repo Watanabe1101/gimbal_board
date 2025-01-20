a9e9a383962864b9b820958e704691550e5f704c
Watanabe1101 <watanabe.s.ca@m.titech.ac.jp>
2025-01-21 00:03:50 +0900

    [format]

--- main/GPTimer/gptimer.h
+++ main/GPTimer/gptimer.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include "driver/gptimer.h"
-//APBクロック80Mhzを使用
-class GPTimer {
+// APBクロック80Mhzを使用
+class GPTimer
+{
 private:
     gptimer_handle_t timer;
     gptimer_alarm_config_t alarm_config;
@@ -11,13 +12,15 @@ private:
 public:
     GPTimer() : timer(nullptr), userCallback(nullptr) {}
 
-    bool init(uint32_t resolution_hz, uint64_t alarm_count) {
+    bool init(uint32_t resolution_hz, uint64_t alarm_count)
+    {
         gptimer_config_t timer_config = {
             .clk_src = GPTIMER_CLK_SRC_DEFAULT,
             .direction = GPTIMER_COUNT_UP,
             .resolution_hz = resolution_hz,
         };
-        if (gptimer_new_timer(&timer_config, &timer) != ESP_OK) {
+        if (gptimer_new_timer(&timer_config, &timer) != ESP_OK)
+        {
             return false;
         }
 
@@ -25,38 +28,41 @@ public:
             .alarm_count = alarm_count,
             .reload_count = 0,
             .flags = {
-                .auto_reload_on_alarm = true
-            }
-        };
-        if (gptimer_set_alarm_action(timer, &alarm_config) != ESP_OK) {
+                .auto_reload_on_alarm = true}};
+        if (gptimer_set_alarm_action(timer, &alarm_config) != ESP_OK)
+        {
             return false;
         }
 
         return true;
     }
 
-    bool registerCallback(gptimer_alarm_cb_t callback) {
+    bool registerCallback(gptimer_alarm_cb_t callback)
+    {
         userCallback = callback;
         gptimer_event_callbacks_t cbs = {
-            .on_alarm = internalCallback
-        };
+            .on_alarm = internalCallback};
         return (gptimer_register_event_callbacks(timer, &cbs, this) == ESP_OK);
     }
 
-    bool start() {
+    bool start()
+    {
         return (gptimer_enable(timer) == ESP_OK && gptimer_start(timer) == ESP_OK);
     }
 
-    bool stop() {
+    bool stop()
+    {
         // タイマーを停止し、無効化する
         bool stopped = (gptimer_stop(timer) == ESP_OK);
         return (stopped && gptimer_disable(timer) == ESP_OK);
     }
 
 private:
-    static bool IRAM_ATTR internalCallback(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_ctx) {
-        GPTimer* gpTimer = static_cast<GPTimer*>(user_ctx);
-        if (gpTimer->userCallback) {
+    static bool IRAM_ATTR internalCallback(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_ctx)
+    {
+        GPTimer *gpTimer = static_cast<GPTimer *>(user_ctx);
+        if (gpTimer->userCallback)
+        {
             return gpTimer->userCallback(timer, edata, user_ctx);
         }
         return false;
