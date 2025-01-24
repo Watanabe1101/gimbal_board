9bd2bcee4f817633c8432fab250e922aeba523f2
Watanabe1101 <watanabe.s.ca@m.titech.ac.jp>
2025-01-24 23:25:44 +0900

    [fix] max device number

--- main/SPICREATE/SPICREATE.hpp
+++ main/SPICREATE/SPICREATE.hpp
@@ -11,11 +11,11 @@
 class SPICreate
 {
     spi_bus_config_t bus_cfg = {};
-    spi_device_handle_t handle[3];
+    spi_device_handle_t handle[4];
     int deviceNum{0};
     spi_host_device_t host = SPI2_HOST;
-    uint8_t mode = 3; // must be 1 or 3
-    int max_size{4094};      // default size
+    uint8_t mode = 3;   // must be 1 or 3
+    int max_size{4094}; // default size
     uint32_t frequency{SPI_MASTER_FREQ_8M};
 
     std::deque<spi_transaction_t> transactions;
@@ -23,9 +23,9 @@ class SPICreate
 
 public:
     bool begin(spi_host_device_t host_in = SPI2_HOST,
-               int sck = -1, 
-               int miso = -1, 
-               int mosi = -1, 
+               int sck = -1,
+               int miso = -1,
+               int mosi = -1,
                uint32_t f = SPI_MASTER_FREQ_8M);
 
     bool end();
